a8a32a542c7d1863fe60c1b096e84230cc55059f
php 19 <php.19@mandachuva.cec.ime.usp.br>
2019-02-06 22:41:25 -0200

    projeto-final-IMEUSP-2019

---
+++ src/Entity/Cliente.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Entity;
+
+class Cliente {
+
+   protected $id;
+   protected $nome;
+   protected $email;
+   protected $telefone;
+   protected $senha;
+
+public Function getId(){
+return $this->id;
+}
+
+public Function getId(){
+return $this->nome;
+}
+
+
+public Function getId(){
+return $this->email;
+}
+
+
+public Function getId(){
+return $this->telefone;
+}
+
+
+public Function getSenha(){
+return $this->senha;
+}
+
+
+
+
+
+
+
+
+}
+
+
+
